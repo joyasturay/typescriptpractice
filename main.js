ad2e64ea6659749f8ffb4053ab8cb1ef340458e1
joyasturay <joyasturay1@gmail.com>
2024-02-22 18:26:54 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,3 @@
+document.getElementById("pname").innerHTML = "Samsung Galaxy Note 7";
+document.getElementById("pPrice").innerHTML = "799";
+document.getElementById("pAvailable").innerHTML = "In Stock"
